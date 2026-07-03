0ea549c03434914e8a08f3e6f110247ded4ca4ae
Debananda-Kuanr <218771338+Debananda-Kuanr@users.noreply.github.com>
2026-07-03 17:26:00 +0530

    Till Day 12

--- DAY 12/test.py
+++ DAY 12/test.py
@@ -98,3 +98,8 @@
 # print(a.intersection(b)) # Using Method 
 # print(a&b) # Using Operator
 
+
+a={1,2,3,4,5,6}
+b={5,6,7,8,9,10}
+print(a.difference(b)) # Using the Method 
+print(a-b) # Using the Operator
